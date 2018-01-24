ed40ee3e18c83ecc4634a68bc0857b1062ad98b7
Juanma <aikijuanma@protonmail.ch>
2018-01-24 01:17:33 +0100

    Update

--- index.md
+++ index.md
@@ -1,9 +1,9 @@
 ---
 layout: default
 date: 2017-09-27
-last_modified_at: 2017-09-27
+last_modified_at: 2018-01-24
 ---
-I'm an independent programmer from Barcelona. I build public and private web applications for companies and institutions. Also, I write [code] for fun. ¶ I’ve had the privilege of working with many great collaborators, like [Barcelona Academy of Art][1], [DeHavilland Ediciones][2], [Etnia Barcelona][3], [La Charca Literaria][4], [Masterbloggen][5], [Micuento][6], and [Prospera Stiftelsen][7]. ¶ Recently, my focus has been on atomic design, critical making, digital humanities, re-decentralizing the Web, and how design and code can come together to create really beautiful experiences. ¶ If you’d like to get in touch about working together, feel free to email me at [hello@juanma.xyz](mailto:hello@juanma.xyz) ([0x5a7588238476115c](https://hkps.pool.sks-keyservers.net/pks/lookup?op=get&search=0x5a7588238476115c)).
+I'm an independent programmer from Barcelona. I build public and private web applications for companies and institutions. Also, I write [code] for fun. ¶ I’ve had the privilege of working with many great collaborators, like [Barcelona Academy of Art][1], [DeHavilland Ediciones][2], [Etnia Barcelona][3], [La Charca Literaria][4], [Masterbloggen][5], [Micuento][6], and [Prospera Stiftelsen][7]. ¶ Recently, my focus has been on atomic design, critical making, digital humanities, re-decentralizing the Web, and how design and code can come together to create really beautiful experiences. ¶ Feel free to email me at [hello@juanma.xyz](mailto:hello@juanma.xyz) ([0x5a7588238476115c](https://hkps.pool.sks-keyservers.net/pks/lookup?op=get&search=0x5a7588238476115c)).
 
 [1]: http://academyofartbarcelona.com
 [2]: http://dehavilland.co
--- scripts/default.js
+++ scripts/default.js
@@ -3,16 +3,6 @@
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 
-  function initPopover() {
-
-    $('aside > small').popover({
-      content: "I'm a human.",
-      placement: 'top',
-      template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-body"></div></div>',
-      trigger: 'hover'
-    });
-  }
-
   function randomTitle() {
 
     var something = new Array(
@@ -34,34 +24,6 @@
     $('body').removeClass('hide').addClass('fadeIn');
   }
 
-  function stopHeartbeat() {
-
-    $('aside > small > a').hover(
-      function() {
-        $(this).removeClass('heartbeat');
-      }, function() {
-        $(this).addClass('heartbeat');
-      }
-    );
-  }
-
-  function updateImageSize() {
-
-    $('article > figure').imagefit();
-  }
-
-  function updateFigureHeight() {
-
-    var imageHeight = $('.img-fluid').height();
-    $('article > figure').css({ 'height' : imageHeight + 'px' });
-  }
-
-  function updateArticleHeight() {
-
-    var imageHeight = $('.img-fluid').height();
-    $('article').css({ 'height' : imageHeight + (imageHeight / 2) + 'px', 'line-height' : imageHeight + (imageHeight / 2) + 'px' });
-  }
-
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
@@ -71,47 +33,9 @@ jQuery(document).ready(function ($) {
 
   randomTitle();
   replacePilcrow();
-  //stopHeartbeat();
-  initPopover();
-
-  // waitForImages
-  // https://github.com/alexanderdickson/waitForImages
-  $('body').waitForImages().done(function() {
-
-    revealContent();
-    updateImageSize();
-    updateFigureHeight();
-    updateArticleHeight();
-  });
-
-
-  if ($(window).width() < 844.24 + 158) {
-
-    updateFigureHeight();
-  }
-  if ($(window).width() < 844.24 + 32) {
-
-    updateArticleHeight();
-  }
-  else {
-  }
-
-  $(window).resize(function () {
-
-    if ($(window).width() < 844.24 + 158) {
-
-      updateFigureHeight();
-    }
-    if ($(window).width() < 844.24 + 32) {
-
-      updateArticleHeight();
-    }
-    else {
-    }
-  });
+  revealContent();
 
   $(window).on('load', function () {
-
   });
 
 });
