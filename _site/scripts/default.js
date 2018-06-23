69c20c3afa3001bacb385b6c8856523b9fdd9aac
Juanma <aikijuanma@protonmail.ch>
2018-06-23 20:03:36 +0200

    Build

--- _site/scripts/default.js
+++ _site/scripts/default.js
@@ -94,9 +94,12 @@
     safariPinnedTab.color = '#000000';
     document.getElementsByTagName('head')[0].appendChild(safariPinnedTab);
 
+    // How to prevent the browser from asking for the favicon?
+    // https://stackoverflow.com/questions/4867206/how-to-prevent-the-browser-from-asking-for-the-favicon
     var favicon = document.createElement('link');
     favicon.rel = 'shortcut icon';
-    favicon.href = window.location.origin + '/laundry-care-symbols/' + icon + '/favicon.ico';
+    // favicon.href = window.location.origin + '/laundry-care-symbols/' + icon + '/favicon.ico';
+    favicon.href = window.location.origin + '/empty';
     document.getElementsByTagName('head')[0].appendChild(favicon);
 
     var msapplicationTileColor = document.createElement('meta');
